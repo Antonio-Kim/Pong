ec9b6183495038db5e894fdf75e2358dd0d4dfcd
Antonio Kim <antoniok@sfu.ca>
2024-02-23 21:19:42 -0800

    Setting up SFML window before coding

--- Pong.cpp
+++ Pong.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include "Bat.h"
+#include <sstream>
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	sf::VideoMode vm(1920, 1080);
+	sf::RenderWindow window(vm, "Pong");
+
+	int score = 0;
+	int lives = 3;
+
+	Bat bat(1920 / 2, 1080 - 20);
+	
+	sf::Text hud;
+	sf::Font font;
+	font.loadFromFile("fonts/digital.tff");
+	hud.setFont(font);
+	hud.setCharacterSize(75);
+	hud.setFillColor(sf::Color::White);
+	hud.setPosition(20, 20);
+
+	sf::Clock clock;
+
+	while (window.isOpen()) {
+
+	}
 
 	return 0;
 }
