27dada0b6e546643627b4bd710c1971867025cf2
Antonio Kim <antoniok@sfu.ca>
2024-02-26 19:22:29 -0800

    Project completed

--- Ball.h
+++ Ball.h
@@ -5,7 +5,7 @@ class Ball {
 private:
 	sf::Vector2f m_Position;
 	sf::RectangleShape m_Shape;
-	float m_Speed = 300.0f;
+	float m_Speed = 1300.0f;
 	float m_DirectionX = .2f;
 	float m_DirectionY = .2f;
 	
